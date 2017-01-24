6d0e15dffc977cec1a8bbc7a7ac96a720d5ff555
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-01-24 20:23:10 +0300

    Assignment 9: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,16 +2,14 @@ var points = document.getElementsByClassName('point');
 
 var animatePoints = function (points) {
     
-    var revealPoint= function (i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebKitTransform = "scaleX(1) translateY(0)";
+    var revealPoint= function (point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebKitTransform = "scaleX(1) translateY(0)";
     };             
 
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }    
+    forEach(points, revealPoint);
                  
 };
 
