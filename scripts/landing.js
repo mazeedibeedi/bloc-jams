8e292979029f5913a7695d61fcf4176c37085ea7
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-01-24 19:45:25 +0300

    Checkpoint 9: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function () {
-                 
-    var points = document.getElementsByClassName('point');
+var points = document.getElementsByClassName('point');
+
+var animatePoints = function (points) {
     
     var revealPoint= function (i) {
         points[i].style.opacity = 1;
@@ -14,3 +14,16 @@ var animatePoints = function () {
     }    
                  
 };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(points);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(points);
+        }
+    });
+}
